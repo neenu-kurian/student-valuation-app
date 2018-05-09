a59c632ef5ce32826f6eb15d64506a424650deba
neenu <neenukurian92@gmail.com>
2018-05-09 13:00:48 +0200

    added logic to display progrees percentage

--- client/src/components/games/GameDetails.jsx
+++ client/src/components/games/GameDetails.jsx
@@ -4,11 +4,11 @@ import {Redirect} from 'react-router-dom'
 import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import { LinearProgress } from 'material-ui/Progress';
+import {withStyles} from 'material-ui/styles';
+import {LinearProgress} from 'material-ui/Progress';
 import Card, {CardActions, CardContent} from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
-import {getStudentsByBatch,deleteStudent} from '../../actions/games'
+import {getStudentsByBatch, deleteStudent} from '../../actions/games'
 import {getUsers} from '../../actions/users'
 import {Link} from 'react-router-dom'
 import './GameDetails.css'
@@ -22,74 +22,73 @@ const styles = theme => ({
   }
 })
 
-
 class GamesDetails extends PureComponent {
-  constructor(){
+  constructor() {
     super()
-    this.handleClick=this.handleClick.bind(this)
+    this.handleClick = this
+      .handleClick
+      .bind(this)
   }
 
   componentWillMount() {
-    
+
     this
       .props
       .getStudentsByBatch(this.props.match.params.id)
     if (this.props.users === null) 
       this.props.getUsers()
-   
-     
+
   }
-  
-  handleClick(id){
-   this.props.deleteStudent(id)
-  
+
+  handleClick(id) {
+    this
+      .props
+      .deleteStudent(id)
+
   }
 
   renderStudents = (student) => {
     console.log('inside renderstudents')
     const {users, history} = this.props
-    
-        
-    
 
     return (
       <div>
 
-     
-      <Card key={student.id} className="student-card">
+        <Card key={student.id} className="student-card">
 
-        <CardContent >
-          <br/>
-          <Typography component="h1">
-            Name:{student.studentname}
-          </Typography>
-          <br/>
-          <Typography component="h1">
-            <img
-              style={{
-              maxHeight: '100px'
-            }}
-              src={student.studentimage}/>
-          </Typography>
-          <br/>
-          <Typography component="h1">
-            {student.evaluation}
-          </Typography>
-          <br/>
-          <Button color="primary"
-        variant="raised"
-        type="submit"
-       
-        onClick={()=>this.handleClick(student.id)}
-        className="delete-student">Delete</Button>
-        </CardContent>
+          <CardContent >
+            <br/>
+            <Typography component="h1">
+              Name:{student.studentname}
+            </Typography>
+            <br/>
+            <Typography component="h1">
+              <img
+                style={{
+                maxHeight: '100px'
+              }}
+                src={student.studentimage}/>
+            </Typography>
+            <br/>
+            <Typography component="h1">
+              {student.evaluation}
+            </Typography>
+            <br/>
+            <Button
+              color="primary"
+              variant="raised"
+              type="submit"
+              onClick={() => this.handleClick(student.id)}
+              className="delete-student">Delete</Button>
+          </CardContent>
 
-      </Card>
-</div>
+        </Card>
+      </div>
     )
   }
 
   render() {
+
     const {students, users, authenticated, classes} = this.props
 
     console.log('inside render')
@@ -99,29 +98,68 @@ class GamesDetails extends PureComponent {
     if (students === null || users === null) 
       return null
 
+    const totallength = students.filter(function (student) {
+      return student
+    })
+      .length
+
+    const greenlength = students.filter(function (student) {
+      return student.evaluation === 'green'
+    })
+      .length
+
+    const yellowlength = students.filter(function (student) {
+      return student.evaluation === 'yellow'
+    })
+      .length
+
+    const redlength = students.filter(function (student) {
+      return student.evaluation === 'red'
+    }).length
+    
+    
+    const greenpercentage = (totallength>0)? (((greenlength / totallength) * 100).toFixed(2)):0
+    const yellowpercentage = (totallength>0)? (((yellowlength / totallength) * 100).toFixed(2)):0
+    const redpercentage = (totallength>0)? (((redlength / totallength) * 100).toFixed(2)):0
+    
+    
     return (
       <div>
-      <LinearProgress variant="determinate" value="10" className={classes.passstyle}/>      
-      <Paper className="outer-paper">
-      
+        <p>GREEN:{greenpercentage}%</p>
+        <LinearProgress
+          variant="determinate"
+          value={greenpercentage}
+          className={classes.passstyle}/>
+        <p>RED:{redpercentage}%</p>
+        <LinearProgress
+          variant="determinate"
+          value={redpercentage}
+          className={classes.passstyle}/>
+        <p>YELLOW:{yellowpercentage}%</p>
+        <LinearProgress
+          variant="determinate"
+          value={yellowpercentage}
+          className={classes.passstyle}/>
 
-        <div>
-          {students.map(student => this.renderStudents(student))}
-        </div>
-        
-       
-       <Link to ={`/newstudent/${this.props.match.params.id}`}> <Button
-        color="primary"
-        variant="raised"
-        type="submit"
-        className="create-student"
-        
-      > Add Student </Button></Link>
-      </Paper>
+        <Paper className="outer-paper">
+
+          <div>
+            {students.map(student => this.renderStudents(student))}
+          </div>
+
+          <Link to ={`/newstudent/${this.props.match.params.id}`}>
+            <Button
+              color="primary"
+              variant="raised"
+              type="submit"
+              className="create-student">
+              Add Student
+            </Button>
+          </Link>
+        </Paper>
       </div>
     )
 
-   
   }
 }
 
@@ -134,4 +172,4 @@ const mapStateToProps = (state, props) => ({
   students: state.students
 })
 
-export default withStyles(styles)(connect(mapStateToProps, {getUsers, getStudentsByBatch,deleteStudent})(GamesDetails))
+export default withStyles(styles)(connect(mapStateToProps, {getUsers, getStudentsByBatch, deleteStudent})(GamesDetails))
